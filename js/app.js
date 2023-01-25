87af47b0706492f2376f942793995a8f9f544249
Geraldo Júnior <geraldo.gj63@gmail.com>
2023-01-24 23:14:10 -0300

    criando uma página de login e cadastro

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// const sectionPrincipal = document.querySelector("section-principal");
+// const cadastrarHeader = document.querySelector(".form-cadastrar header");
+// const loginHeader = document.querySelector(".form-login header");
+
+// loginHeader.addEventListener("click", () => {
+//     sectionPrincipal.classList.add("active");
+// });
+// cadastrarHeader.addEventListener("click", () => {
+//     sectionPrincipal.classList.add("active");
+// });
